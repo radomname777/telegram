ebcf28cdf27f359abc1f88d9f8b8330b8ed9001c
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-06-18 17:10:08 +0400

    0.8

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -50,7 +50,6 @@ namespace WpfApp2
             else if(!isokay) Txt_box.Text = "Write a message...";
 
         }
-
         private void Txt_box_MouseMove(object sender, MouseEventArgs e)
         {
             isokay = true;
@@ -61,14 +60,78 @@ namespace WpfApp2
             if (Txt_box.Text.Length == 0)Txt_box.Text = "Write a message...";
             isokay = false;
         }
+        int num2 = 0;
+        private List<Label> Search_Grid(Grid a, string data)
+        {
+            List<Label> list = new List<Label>();
+            foreach (var item in a.Children)
+            {
+              //  MessageBox.Show(item.ToString());
+                if (item is Label lbl)
+                {
+                    //   MessageBox.Show(lbl.Content.ToString());
+                    if (lbl.FontSize == 18)
+                    {
+                      //  MessageBox.Show(lbl.Content.ToString());
+                        string str = lbl.Content.ToString();
+                        if (str.IndexOf(data) != -1)
+                        {
 
+                            num2++;
+                            list.Add(lbl);
+                           
+                            // MessageBox.Show(num2.ToString());
+                        }
+                        else
+                        {
+                            // MessageBox.Show(str.IndexOf(data).ToString());
+                        }
+
+                    }
+                    //else { }
+                    //MessageBox.Show("a");
+                }
+            }
+         
+
+            return list;
+        }
         private void Search_PreviewKeyUp(object sender, KeyEventArgs e)
         {
-            //List<Grid> list = new List<Grid>();
+            List<Label> list = new List<Label>();
             Grid a = new Grid();
             a = Usergrid;
-            Gogrid.Children.Remove(Usergrid);
-            MessageBox.Show("a");
+
+            //Gogrid.Children.Remove(Usergrid);
+            //Usergrid.Children.Clear();
+            foreach (var item in Usergrid.Children)
+            {
+                if (item is Label l)
+                {
+                    Usergrid.Children.Remove(l);
+
+                }
+            }
+            if (Search.Text.Length != 0)
+            {
+
+                list = Search_Grid(a, Search.Text);
+            }
+            foreach (var item in list)
+            {
+                if (item is Label l )
+                {
+                    Usergrid.Children.Add(l);
+                }
+            }
+            Gogrid.Children.Add(Usergrid);
+            //    foreach (var item in list)
+            //    {
+            //        MessageBox.Show(item.ToString());
+            //        Usergrid.Children.Add(item);
+            //    }
+            //}
+           // MessageBox.Show("a");
             //Thread.Sleep(1000);
             //Gogrid.Children.Add(a);
             //foreach (var item in Usergrid.Children)
