b73ae7cc306d719eb300848a022daf3619ae1154
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-06-18 19:06:18 +0400

    Finsh

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -16,38 +16,38 @@ using System.Windows.Shapes;
 
 namespace WpfApp2
 {
-
     public partial class MainWindow : Window
     {
         public bool isokay { get; set; } = false;
+        List<Grid> gr0 = new List<Grid> { };
         public MainWindow()
         {
             InitializeComponent();
+            AddFolder();
             Messagepanel.VerticalAlignment = VerticalAlignment.Bottom;
             num++;
         }
-
         int num = 1;
         private void del()
         {
             foreach (var item in Messagepanel.Children)
-                if (item is Label txt){Messagepanel.Children.Remove(txt); return; }
+                if (item is Label txt) { Messagepanel.Children.Remove(txt); return; }
         }
         private void TextBox_PreviewKeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter&&isokay&& Txt_box.Text.Length!=0)
+            if (e.Key == Key.Enter && isokay && Txt_box.Text.Length != 0)
             {
                 var textBox = sender as TextBox;
-                if (num == 1){del();num--;}
+                if (num == 1) { del(); num--; }
                 Label label = new Label();
                 label.Content = textBox.Text;
                 label.Background = Brushes.White;
                 label.HorizontalAlignment = HorizontalAlignment.Right;
                 label.VerticalAlignment = VerticalAlignment.Bottom;
-                Messagepanel.Children.Add(label);num++;
+                Messagepanel.Children.Add(label); num++;
                 Txt_box.Text = "";
             }
-            else if(!isokay) Txt_box.Text = "Write a message...";
+            else if (!isokay) Txt_box.Text = "Write a message...";
 
         }
         private void Txt_box_MouseMove(object sender, MouseEventArgs e)
@@ -57,87 +57,69 @@ namespace WpfApp2
         }
         private void Grid_MouseMove(object sender, MouseEventArgs e)
         {
-            if (Txt_box.Text.Length == 0)Txt_box.Text = "Write a message...";
+            if (Txt_box.Text.Length == 0) Txt_box.Text = "Write a message...";
             isokay = false;
         }
-        int num2 = 0;
-        private List<Label> Search_Grid(Grid a, string data)
+        private void AddFolder()
         {
-            List<Label> list = new List<Label>();
-            foreach (var item in a.Children)
+            foreach (var item in Usergrid.Children)
             {
-              //  MessageBox.Show(item.ToString());
-                if (item is Label lbl)
-                {
-                    //   MessageBox.Show(lbl.Content.ToString());
-                    if (lbl.FontSize == 18)
-                    {
-                      //  MessageBox.Show(lbl.Content.ToString());
-                        string str = lbl.Content.ToString();
-                        if (str.IndexOf(data) != -1)
-                        {
-
-                            num2++;
-                            list.Add(lbl);
-                           
-                            // MessageBox.Show(num2.ToString());
-                        }
-                        else
-                        {
-                            // MessageBox.Show(str.IndexOf(data).ToString());
-                        }
-
-                    }
-                    //else { }
-                    //MessageBox.Show("a");
-                }
+                if (item is Grid grids)
+                    gr0.Add(grids);
             }
-         
-
-            return list;
+        }
+        List<Grid> gr1 = new List<Grid> { };
+        private bool IsOkay(string text, Grid gri)
+        {
+            if (Search.Text.Length != 0 && text.IndexOf(Search.Text) != -1)
+            {
+                gr1.Add(gri);
+                return true;
+            }
+            return false;
+        }
+        private void beraber()
+        {
+            Usergrid.Children.Clear();
+            foreach (var item in gr0)
+            {
+                Usergrid.Children.Add(item);
+            }
         }
         private void Search_PreviewKeyUp(object sender, KeyEventArgs e)
         {
-            List<Label> list = new List<Label>();
-            Grid a = new Grid();
-            a = Usergrid;
-
-            //Gogrid.Children.Remove(Usergrid);
-            //Usergrid.Children.Clear();
-            foreach (var item in Usergrid.Children)
+            if (e.Key != Key.Enter)
             {
-                if (item is Label l)
+                if (Search.Text.Length==0)
                 {
-                    Usergrid.Children.Remove(l);
-
+                    beraber();
+                    return;
                 }
-            }
-            if (Search.Text.Length != 0)
-            {
-
-                list = Search_Grid(a, Search.Text);
-            }
-            foreach (var item in list)
-            {
-                if (item is Label l )
+                gr1.Clear();
+                bool boolen = false;
+                int nums = 0;
+                foreach (Grid item in gr0)
                 {
-                    Usergrid.Children.Add(l);
+                    foreach (var items in item.Children)
+                    {
+                        if (items is Label lbl)
+                        {
+                            if (lbl.FontSize == 18 && IsOkay(lbl.Content.ToString(), item)) {
+                                boolen = true; nums++; }
+                        }
+                    }
                 }
+                if (boolen)
+                {
+                    Usergrid.Children.Clear();
+                    foreach (var item in gr1)
+                    {
+                        Usergrid.Children.Add(item);
+                        num--;
+                    }
+                }
+
             }
-            Gogrid.Children.Add(Usergrid);
-            //    foreach (var item in list)
-            //    {
-            //        MessageBox.Show(item.ToString());
-            //        Usergrid.Children.Add(item);
-            //    }
-            //}
-           // MessageBox.Show("a");
-            //Thread.Sleep(1000);
-            //Gogrid.Children.Add(a);
-            //foreach (var item in Usergrid.Children)
-            //{
-            //    list.Add
-            //}
         }
     }
 }
